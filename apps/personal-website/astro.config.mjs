0d8b902f30763cf6348dd67372c3870b43d7a31f
Rainforest <rainforestnick@gmail.com>
2023-12-03 07:54:32 +0000

    feat: add @astro/node

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -3,6 +3,7 @@ import lit from '@astrojs/lit';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
+// import node from '@astrojs/node';
 
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
@@ -13,10 +14,15 @@ import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   vite: {
     // https://docs.astro.build/en/guides/troubleshooting/#adding-dependencies-to-astro-in-a-monorepo
     ssr: {
       noExternal: [
+        // '@astrojs/node',
         '@astrojs/lit',
         '@astrojs/react',
         '@astrojs/svelte',
