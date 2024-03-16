71aeec8f44488ac6f99e9835fc0ba4d5dcb60bb8
Rainforest Cheng <rainforestnick@gmail.com>
2024-03-16 14:42:18 +0000

    refactor: use @nxtensions/astro

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -3,26 +3,19 @@ import lit from '@astrojs/lit';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
-// import node from '@astrojs/node';
-
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import path from 'path';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
   vite: {
     // https://docs.astro.build/en/guides/troubleshooting/#adding-dependencies-to-astro-in-a-monorepo
     ssr: {
       noExternal: [
-        // '@astrojs/node',
         '@astrojs/lit',
         '@astrojs/react',
         '@astrojs/svelte',
