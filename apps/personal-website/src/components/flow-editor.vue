93ab2ece17d3865c467ee2a6ed2fc4de2aa2bc2d
Rainforest Cheng <rainforestnick@gmail.com>
2024-03-19 15:07:23 +0000

    feat: add Vue Flow controls and update flow editor layout

--- apps/personal-website/src/components/flow-editor.vue
+++ apps/personal-website/src/components/flow-editor.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="flex items-center justify-center size-full">
-    <VueFlow :nodes="nodes" :edges="edges">
+    <VueFlow :nodes="nodes" fit-view-on-init>
+      <Controls />
     </VueFlow>
   </div>
 </template>
 <script setup lang="ts">
-import type { Elements } from '@vue-flow/core'
-import { VueFlow, useVueFlow } from '@vue-flow/core'
+import type { Elements, Node } from '@vue-flow/core';
+import { VueFlow, useVueFlow } from '@vue-flow/core';
+import { Controls } from '@vue-flow/controls';
 import { ref } from 'vue';
+import { useMouse } from '@vueuse/core';
 
-const nodes = ref<Elements>([
+const { screenToFlowCoordinate, onConnect, addEdges, addNodes, onPaneClick } = useVueFlow();
+
+onConnect(addEdges);
+onPaneClick((event) => {
+  const position = screenToFlowCoordinate({ x: event.clientX, y: event.clientY });
+  const id = String(Date.now());
+  addNodes([
+    {
+      id,
+      position,
+      label: `Node ${id}`,
+    },
+  ]);
+});
+
+const { x, y } = useMouse();
+
+const nodes = ref<Node[]>([
   {
     id: '1',
-    label: 'node 1',
-    position: { x: 100, y: 100 },
+    position: { x: 50, y: 50 },
+    label: 'Node 1',
   },
-  {
-    id: '2',
-    label: 'node 2',
-    position: { x: 100, y: 200 },
-  },
-])
-
-const edges = ref<Elements>([
-  {
-    id: 'e1-2',
-    target: '2',
-    source: '1',
-  },
-])
+]);
 
 </script>
 <style>
 @import '@vue-flow/core/dist/style.css';
 @import '@vue-flow/core/dist/theme-default.css';
+@import '@vue-flow/controls/dist/style.css';
 </style>
