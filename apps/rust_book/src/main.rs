0acd977eb5a685241e1a6b4dda6a15c68755cdb2
Rainforest Cheng <rainforestnick@gmail.com>
2024-04-21 06:01:22 +0000

    Add User struct and create a user instance

--- apps/rust_book/src/main.rs
+++ apps/rust_book/src/main.rs
@@ -1,4 +1,15 @@
+struct User {
+    name: String,
+    active: bool,
+    age: u32,
+}
+
 fn main() {
+    let user = User {
+        name: String::from("John"),
+        active: true,
+        age: 30,
+    };
     println!("Hello, world!");
 }
 
