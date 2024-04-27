66a0ede90c89dbf3a7cbe6b3c2e21597ec2854c1
Rainforest Cheng <rainforestnick@gmail.com>
2024-04-27 11:04:13 +0000

    feat(personal-website): add tailwindcss v4 support

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -5,23 +5,13 @@ import vue from '@astrojs/vue';
 import svelte from '@astrojs/svelte';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { fileURLToPath } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/personal-website',
-  integrations: [
-    lit(),
-    react(),
-    vue(),
-    svelte(),
-    partytown(),
-    sitemap(),
-    tailwind({
-      configFile: fileURLToPath(
-        new URL('./tailwind.config.cjs', import.meta.url)
-      ),
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [lit(), react(), vue(), svelte(), partytown(), sitemap()],
 });
