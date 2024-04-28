3ff697c998c0009edd4f63c06583244ec2ce2ec7
Rainforest Cheng <rainforestnick@gmail.com>
2024-04-28 08:28:48 +0000

    feat(personal-backend): init elysia

--- apps/personal-backend/src/main.ts
+++ apps/personal-backend/src/main.ts
@@ -1,7 +1,32 @@
-Bun.serve({
-  port: 8080,
-  async fetch(request: Request): Promise<Response> {
-    return new Response('Hello World!');
-  },
-});
-console.log('running on port http://localhost:8080');
+import { Elysia, t } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
+import { serverTiming } from '@elysiajs/server-timing';
+import pkg from '../../../package.json';
+import { PORT } from './constants';
+import common from './controller';
+
+const app = new Elysia()
+  .use(cors())
+  .use(
+    swagger({
+      path: '/docs',
+      documentation: {
+        info: {
+          title: 'Personal Backend API',
+          version: pkg.version,
+        },
+      },
+    }),
+  )
+  .use(serverTiming());
+
+// use controllers
+app.use(common);
+
+// start server
+app.listen(PORT);
+
+console.info(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
