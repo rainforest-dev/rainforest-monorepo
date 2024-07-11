3ebd95e5e02a27f87eeb934a30b373fce2017e5f
Rainforest Cheng <rainforestnick@gmail.com>
2024-07-11 09:06:20 +0000

    feat(nx): Added Nx Cloud token to your nx.json
              To connect your workspace to Nx Cloud, push your repository
              to your git hosting provider and go to the following URL:
    
              https://cloud.nx.app/connect/36abtP3AJM

--- apps/personal-backend/src/libs/utils.ts
+++
@@ -1,7 +0,0 @@
-export const uint8ArrayToBase64 = (u8a: Uint8Array) => {
-  // return in web-safe base64
-  return btoa(String.fromCharCode(...u8a))
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
-};
