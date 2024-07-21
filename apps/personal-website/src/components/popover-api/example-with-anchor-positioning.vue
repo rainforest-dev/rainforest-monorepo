f040b608bf18a75f824bc2e941f3f7e1d47d163a
Rainforest Cheng <rainforestnick@gmail.com>
2024-07-21 11:16:42 +0000

    feat(personal-website): update blog about  popover API

---
+++ apps/personal-website/src/components/popover-api/example-with-anchor-positioning.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full border h-40 flex items-center justify-center *:border relative">
+    <button :popovertarget="name">Open Popover</button>
+
+    <div :id="name" popover>
+      <p>
+        I am a popover with more information. Hit <kbd>esc</kbd> or click away to
+        close me.
+      </p>
+    </div>
+  </div>
+</template>
+<script setup>
+const name = 'example-with-anchor-positioning'
+</script>
+<style scoped>
+button {
+  anchor-name: --anchor;
+}
+
+[popover] {
+  inset: auto;
+  position: absolute;
+  position-anchor: --anchor;
+  bottom: anchor(top);
+  left: anchor(right);
+
+}
+</style>
---
+++ apps/personal-website/src/components/popover-api/example-with-popover-api.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full border h-40 flex items-center justify-center *:border">
+    <button popovertarget="my-popover">Open Popover</button>
+
+    <div id="my-popover" popover class="top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+      <p>
+        I am a popover with more information. Hit <kbd>esc</kbd> or click away to
+        close me.
+      </p>
+    </div>
+  </div>
+</template>
