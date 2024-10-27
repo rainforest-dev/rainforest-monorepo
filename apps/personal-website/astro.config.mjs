038ae268a83ecf577e606f09c65ed3237f7988cb
Rainforest Cheng <rainforestnick@gmail.com>
2024-10-27 06:46:15 +0000

    feat(personal-website): Add support for Lit, React, Vue, and MDX integrations in Astro config

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
 
+import lit from '@astrojs/lit';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [lit(), react(), vue(), mdx()]
 });
