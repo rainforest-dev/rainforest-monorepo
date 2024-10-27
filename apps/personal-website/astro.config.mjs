66edd7631e5c904e37500b5a41191182571e3611
Rainforest Cheng <rainforestnick@gmail.com>
2024-10-27 03:00:28 +0000

    migrate to nx 20

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -1,26 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import lit from '@astrojs/lit';
-import react from '@astrojs/react';
-import vue from '@astrojs/vue';
-import mdx from '@astrojs/mdx';
-// import tailwind from '@astrojs/tailwind';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({
-  outDir: '../../dist/apps/personal-website',
-  integrations: [
-    lit(),
-    react(),
-    vue({
-      appEntrypoint: '/src/pages/_app',
-    }),
-    mdx(),
-    // tailwind({
-    //   configFile: 'apps/personal-website/tailwind.config.mjs',
-    // }),
-  ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+export default defineConfig({});
--- apps/personal-website/src/components/popover-api/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ExampleWithPopoverApi } from './example-with-popover-api.vue';
-export { default as ExampleWithAnchorPostioning } from './example-with-anchor-positioning.vue';
