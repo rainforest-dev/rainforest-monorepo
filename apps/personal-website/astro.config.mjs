b40019ee17269893deeb6def39a0f31442de44c4
Rainforest Cheng <rainforestnick@gmail.com>
2024-10-31 10:26:11 +0000

    feat(personal-website): add Vercel adapter for serverless deployment; update .gitignore to include .vercel

--- apps/personal-website/astro.config.mjs
+++ apps/personal-website/astro.config.mjs
@@ -6,10 +6,21 @@ import lit from '@astrojs/lit';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [lit(), react(), vue()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    isr: true,
+  }),
 });
