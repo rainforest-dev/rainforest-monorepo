8a3fc96d4cc123a7bf2f75e54a4ebb914d796105
Rainforest Cheng <rainforestnick@gmail.com>
2024-11-04 07:33:40 +0000

    refactor(personal-website): refactor color system and theme management; streamline color updates and improve cookie handling

--- apps/personal-website/src/stores/color-system.ts
+++ apps/personal-website/src/stores/color-system.ts
@@ -1,3 +1,27 @@
 import { persistentAtom } from '@nanostores/persistent';
+import Cookies from 'js-cookie';
+import { defaultSourceColor } from '../utils/constants';
 
-export const sourceColor = persistentAtom<string>('source-color', '#66b2b2');
+export const persistentColorSchemeKey = 'dark';
+export const colorScheme = persistentAtom<boolean>(
+  persistentColorSchemeKey,
+  Boolean(Cookies.get(persistentColorSchemeKey)) ?? false,
+  {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+  }
+);
+export const updateColorScheme = (scheme: boolean) => {
+  colorScheme.set(scheme);
+  Cookies.set(persistentColorSchemeKey, String(scheme));
+};
+
+export const persistentKey = 'source-color';
+export const sourceColor = persistentAtom<string>(
+  persistentKey,
+  Cookies.get(persistentKey) ?? defaultSourceColor
+);
+export const updateSourceColor = (color: string) => {
+  sourceColor.set(color);
+  Cookies.set(persistentKey, color);
+};
