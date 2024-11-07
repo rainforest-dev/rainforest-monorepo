4e60d03edaf7eb5a7f39b7fa7ab6b7754e803f0e
Rainforest Cheng <rainforestnick@gmail.com>
2024-11-07 01:54:09 +0000

    refactor(personal-website): update scroll behavior and enhance navigation link functionality

--- apps/personal-website/src/components/home/nav.vue
+++ apps/personal-website/src/components/home/nav.vue
@@ -2,11 +2,23 @@
   <nav class="fixed top-0 inset-x-0 text-on-background h-20 px-10">
     <ul class="flex-row-center justify-end gap-10">
       <li>
-        <a href="#experience">Experience</a>
+        <a href="#experience" @click="handleClick">Experience</a>
       </li>
       <li>
-        <a href="#skills">Skills</a>
+        <a href="#skills" @click="handleClick">Skills</a>
       </li>
     </ul>
   </nav>
 </template>
+<script lang="ts" setup>
+const handleClick = () => {
+  // remove the anchor part of the URL after clicking on a link after 300ms
+  requestAnimationFrame(() => {
+    window.history.replaceState(
+      null,
+      '',
+      window.location.href.replace(window.location.hash, '')
+    );
+  });
+};
+</script>
