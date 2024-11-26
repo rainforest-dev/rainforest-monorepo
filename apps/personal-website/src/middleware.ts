b5c4a74cc9af37e41bd7fce48f1bfe1dd4810e91
Rainforest Cheng <rainforestnick@gmail.com>
2024-11-26 07:45:51 +0000

    fix(personal-website): improve locale redirection handling in middleware

--- apps/personal-website/src/middleware.ts
+++ apps/personal-website/src/middleware.ts
@@ -7,16 +7,21 @@ const ROUTE_TYPE_HEADER = 'X-Astro-Route-Type' as const;
 export const onRequest = defineMiddleware(async (context, next) => {
   const response = await next();
   const type = response.headers.get(ROUTE_TYPE_HEADER);
-  if (type !== 'page') return response;
-  const preferredLocale =
-    context.cookies.get(persistentLocaleKey)?.value || context.preferredLocale;
-  if (preferredLocale && preferredLocale !== context.currentLocale) {
-    return context.redirect(
-      getRelativeLocaleUrl(
-        preferredLocale,
-        context.url.pathname.replaceAll(`/${context.currentLocale}`, '')
-      )
-    );
+  if (type === 'page') {
+    const preferredLocale =
+      context.cookies.get(persistentLocaleKey)?.value ||
+      context.preferredLocale;
+    if (preferredLocale && preferredLocale !== context.currentLocale) {
+      const pathnameWithoutLocale = context.url.pathname.replaceAll(
+        `/${context.currentLocale}`,
+        ''
+      );
+      if (['/', '/resume'].includes(pathnameWithoutLocale)) {
+        return context.redirect(
+          getRelativeLocaleUrl(preferredLocale, pathnameWithoutLocale)
+        );
+      }
+    }
   }
   return response;
 });
