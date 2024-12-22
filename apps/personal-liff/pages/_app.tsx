6eb1a0e6131cb07307d234afba4a3a5a1c610c1e
Rainforest Cheng <contact@rainforest.tools>
2024-12-22 08:45:25 +0000

    feat(personal-liff): set up initial project structure with environment configuration and ESLint setup

---
+++ apps/personal-liff/pages/_app.tsx
@@ -0,0 +1,48 @@
+import './styles.css';
+
+import type { Liff } from '@line/liff';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { JSX, useEffect, useState } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import('@line/liff')
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log('LIFF init...');
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+          .then(() => {
+            console.log('LIFF init succeeded.');
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            console.log('LIFF init failed.');
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
+
+  // Provide `liff` object and `liffError` object
+  // to page component as property
+  pageProps.liff = liffObject;
+  pageProps.liffError = liffError;
+  return (
+    <>
+      <Head>
+        <title>Welcome to personal-liff!</title>
+      </Head>
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default MyApp;
