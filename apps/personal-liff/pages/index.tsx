003e9ef1512aaa9dd7ed528dc2d59dc12dde3629
Rainforest Cheng <contact@rainforest.tools>
2024-12-23 03:49:54 +0000

    feat(personal-liff): add hooks for LINE profile retrieval, update project structure and configuration

--- apps/personal-liff/pages/index.tsx
+++ apps/personal-liff/pages/index.tsx
@@ -1,40 +1,42 @@
 import type { Liff } from '@line/liff';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
-import styles from './index.module.css';
+import { useLineProfile } from '@/hooks';
 
 const Index: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
   liff,
   liffError,
 }) => {
+  const { profile, isLoading } = useLineProfile();
+
+  console.log(profile, isLoading);
+
   return (
-    <div>
-      <Head>
-        <title>LIFF App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1>create-liff-app</h1>
-        {liff && <p>LIFF init succeeded.</p>}
-        {liffError && (
-          <>
-            <p>LIFF init failed.</p>
-            <p>
-              <code>{liffError}</code>
-            </p>
-          </>
-        )}
-        <a
-          href="https://developers.line.biz/ja/docs/liff/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          LIFF Documentation
-        </a>
-      </main>
-    </div>
+    <>
+      <h1>create-liff-app</h1>
+      {profile && (
+        <>
+          <p>{profile.displayName}</p>
+          <img src={profile.pictureUrl} alt={profile.displayName} />
+        </>
+      )}
+      {liff && <p>LIFF init succeeded.</p>}
+      {liffError && (
+        <>
+          <p>LIFF init failed.</p>
+          <p>
+            <code>{liffError}</code>
+          </p>
+        </>
+      )}
+      <a
+        href="https://developers.line.biz/ja/docs/liff/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LIFF Documentation
+      </a>
+    </>
   );
 };
 
