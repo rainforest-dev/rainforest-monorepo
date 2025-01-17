e4cd3946262263db68937ec5b5f297cf802eef8f
Rainforest Cheng <contact@rainforest.tools>
2025-01-17 08:33:05 +0000

    feat(blog): update quick post keyboard enter

--- apps/personal-website/src/components/blog/demo/keyboard-enter-event.vue
+++ apps/personal-website/src/components/blog/demo/keyboard-enter-event.vue
@@ -1,8 +1,33 @@
 <template>
-  <input placeholder="Search..." @keydown.enter="handleEnter" />
+  <div class="flex flex-col gap-4">
+    <input
+      placeholder="Press any key to test."
+      @keydown="handleKeydown"
+      @focusout="handleFocusout"
+      :inputmode="inputmode"
+      :autocapitalize="autocapitalize"
+    />
+    Last Pressed: {{ lastPressedKey }}
+  </div>
 </template>
 <script setup lang="ts">
-const handleEnter = (e) => {
-  console.log('Enter key pressed', e);
+import { ref } from 'vue';
+
+const autocapitalize = ref<
+  'sentences' | 'words' | 'characters' | 'none' | undefined
+>(undefined);
+const inputmode = ref<'text' | 'numeric' | 'url' | 'email'>('email');
+const lastPressedKey = ref('');
+
+const handleKeydown = (e: KeyboardEvent) => {
+  lastPressedKey.value = e.key;
+  if (e.key === 'Enter') {
+    console.log('Enter key pressed', e);
+  }
+};
+
+const handleFocusout = (e: FocusEvent) => {
+  console.log('Input lost focus', e);
+  lastPressedKey.value = 'focus out';
 };
 </script>
