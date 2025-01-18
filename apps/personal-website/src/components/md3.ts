4e38e65abd6ed4cc855b9cca043c9dbe1995c2ae
Rainforest Cheng <contact@rainforest.tools>
2025-01-18 03:26:00 +0000

    feat(blog): enhance accessibility with enterKeyHint and add outlined input components

--- apps/personal-website/src/components/md3.ts
+++ apps/personal-website/src/components/md3.ts
@@ -1,6 +1,9 @@
 import { createComponent } from '@lit/react';
 import { MdChipSet } from '@material/web/chips/chip-set';
 import { MdFilterChip } from '@material/web/chips/filter-chip';
+import { MdOutlinedSelect } from '@material/web/select/outlined-select';
+import { MdSelectOption } from '@material/web/select/select-option';
+import { MdOutlinedTextField } from '@material/web/textfield/outlined-text-field';
 import React from 'react';
 
 export const ChipSet = createComponent({
@@ -15,5 +18,26 @@ export const FilterChip = createComponent({
   react: React,
   events: {
     onClick: 'click',
+  },
+});
+
+export const OutlinedTextField = createComponent({
+  tagName: 'md-outlined-text-field',
+  elementClass: MdOutlinedTextField,
+  react: React,
+});
+
+export const SelectOption = createComponent({
+  tagName: 'md-select-option',
+  elementClass: MdSelectOption,
+  react: React,
+});
+
+export const OutlinedSelect = createComponent({
+  tagName: 'md-outlined-select',
+  elementClass: MdOutlinedSelect,
+  react: React,
+  events: {
+    onChange: 'change',
   },
 });
