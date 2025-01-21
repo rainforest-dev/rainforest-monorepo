d58eb8435e6162003359200c3b532e0d224fee87
Rainforest Cheng <contact@rainforest.tools>
2025-01-21 08:36:09 +0000

    feat(personal-website): add support for Prompt API in WebLLM component and update dependencies

--- apps/personal-website/src/components/blog/demo/webgpu/web-llm.vue
+++ apps/personal-website/src/components/blog/demo/webgpu/web-llm.vue
@@ -26,16 +26,28 @@
 </template>
 <script lang="ts" setup>
 import { CreateMLCEngine } from '@mlc-ai/web-llm';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { marked } from 'marked';
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/button/filled-button.js';
+
+const { api = 'web-llm' } = defineProps<{ api: 'web-llm' | 'prompt-api' }>();
+
+const isAIFeatureSupported = computed(() => {
+  switch (api) {
+    case 'prompt-api':
+      return 'ai' in self;
+    default:
+      return true;
+  }
+});
+console.log(api, isAIFeatureSupported.value);
 
 const selectedModel = 'Llama-3.2-3B-Instruct-q4f32_1-MLC';
 
 const engine = await CreateMLCEngine(selectedModel, {
   initProgressCallback: ({ progress }) => {
-    console.log(`Initialization progress: ${progress}%`);
+    console.log(`Initialization progress: ${(progress * 100).toFixed()}%`);
   },
 });
 
@@ -43,22 +55,37 @@ const message = ref('');
 const reply = ref('');
 
 const sendMessage = async () => {
-  const messages = [
-    {
-      role: 'user',
-      content: message.value,
-    },
-  ];
-  const chunks = await engine.chat.completions.create({
-    // @ts-ignore
-    messages,
-    stream: true,
-  });
+  switch (api) {
+    case 'web-llm': {
+      const messages = [
+        {
+          role: 'user',
+          content: message.value,
+        },
+      ];
+      const chunks = await engine.chat.completions.create({
+        // @ts-ignore
+        messages,
+        stream: true,
+      });
 
-  reply.value = ''; // Reset the reply before appending new content
-  for await (const chunk of chunks) {
-    reply.value += chunk.choices[0].delta.content || '';
+      reply.value = ''; // Reset the reply before appending new content
+      for await (const chunk of chunks) {
+        reply.value += chunk.choices[0].delta.content || '';
+      }
+      message.value = ''; // Clear the input field after sending the message
+      break;
+    }
+    case 'prompt-api': {
+      if ('ai' in self) {
+        const session = await self.ai.languageModel.create();
+        const stream = await session.promptStreaming(message.value);
+        for await (const chunk of stream) {
+          reply.value += chunk;
+        }
+      }
+      break;
+    }
   }
-  message.value = ''; // Clear the input field after sending the message
 };
 </script>
